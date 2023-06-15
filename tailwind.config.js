1912f68bce9f338b6002a911b45f4414029cdd3c
sluxzer <initaufikhidayat@gmail.com>
2023-06-15 12:10:53 +0700

    Convert Vue to React

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        './resources/js/**/*.tsx',
     ],
 
     theme: {
